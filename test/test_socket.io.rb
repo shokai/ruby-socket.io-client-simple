664a3a053799da48f215a60f0d7cff030da801c3
Sho Hashimoto <hashimoto@shokai.org>
2016-02-18 23:56:00 +0900

    modify TravisCI setting  - update ruby & node version  - run on container
    fixed test
    
    bundle update

--- test/test_socket.io.rb
+++ test/test_socket.io.rb
@@ -1,4 +1,4 @@
-require File.expand_path 'test/test_helper'#, File.dirname(__FILE__)
+require File.expand_path 'test_helper', File.dirname(__FILE__)
 
 class TestSocketIOClientSimple < MiniTest::Test
 
